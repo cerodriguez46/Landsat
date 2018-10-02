ecac06e9d04c6cf3588985f8ef8ff5ef9bede763
Chris Rodriguez <cerdevelopers@gmail.com>
2018-10-02 09:47:45 -0500

    Feat: Set parent activity via the manifest for detail activity and tutorial menu activity

--- app/src/main/java/christopher/landsat3/DetailActivity.java
+++ app/src/main/java/christopher/landsat3/DetailActivity.java
@@ -1,7 +1,7 @@
 package christopher.landsat3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +9,6 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
     }
 }
