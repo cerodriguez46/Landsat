685a36d0514a0f4fd89e399242323703accb8262
Chris Rodriguez <cerdevelopers@gmail.com>
2018-10-11 23:00:31 -0500

    Update: Add Tutorial and Saved Image menus to DetailActivity

--- app/src/main/java/christopher/landsat3/DetailActivity.java
+++ app/src/main/java/christopher/landsat3/DetailActivity.java
@@ -3,6 +3,8 @@ package christopher.landsat3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -71,5 +73,33 @@ public class DetailActivity extends AppCompatActivity {
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.tutorial_menu) {
+            Intent intent = new Intent(DetailActivity.this, Tutorial.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.imgs_saved) {
+            Intent intent = new Intent(DetailActivity.this, Bookmarks.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
