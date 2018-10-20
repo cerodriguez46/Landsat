00afc3e6c4fe4364d326cd523fea58b5e038e7b3
Chris Rodriguez <cerdevelopers@gmail.com>
2018-10-19 23:11:39 -0500

    Feat: Dao class created with query all records, insert a record, update a record, and delete a record

---
+++ app/src/main/java/christopher/landsat3/Data/LandsatDao.java
@@ -0,0 +1,33 @@
+package christopher.landsat3.Data;
+
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+import christopher.landsat3.Networking.LandsatModel;
+
+@Dao
+public interface LandsatDao {
+
+    //loads list of tasks
+    @Query("SELECT * FROM   landsat")
+    List<LandsatModel> loadAllRecords();
+
+    //inserts new record into the database
+    @Insert
+    void insertRecord(LandsatModel landsatModel);
+
+    //update records
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateRecord(LandsatModel landsatModel);
+
+    //delete records
+    @Delete
+    void deleteRecord(LandsatModel landsatModel);
+}
